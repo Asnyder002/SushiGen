6ea670515a8248b308826613f2d4bf2f122de559
Adam Snyder <adam296@live.com>
2019-11-11 14:03:46 -0600

    Everything works for nav bar on all browsers HERE

--- sushiGen.js
+++ sushiGen.js
@@ -1,4 +1,5 @@
 const navSlide = function() {
+    
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
