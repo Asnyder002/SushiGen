59086e7e679ef70f60f3d846f90480b3680df352
Adam Snyder <adam296@live.com>
2019-11-20 23:00:03 -0600

    Added smooth scroll to arrow button click

--- sushiGen.js
+++ sushiGen.js
@@ -28,4 +28,37 @@ const navSlide = function() {
 
 }
 
+const smoothScroll = function(target, duration) {
+
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    const animation = function(currentTime) {
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    const ease = function(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+
+}
+
+const arrow = document.querySelector(".arrow");
+
+arrow.addEventListener("click", function(){
+    smoothScroll(".logoBanner", 100);
+});
+
 navSlide();
